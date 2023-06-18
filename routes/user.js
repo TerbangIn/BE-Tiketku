751d06daa7d4897dc0b74beadd8d8d7277c4fc61
zodplugin <zodplugin@gmail.com>
2023-06-18 23:10:46 +0700

    forget password

--- routes/user.js
+++ routes/user.js
@@ -11,5 +11,6 @@ router.put('/:id', auth, checkRole(["admin", "user"]), userController.updateUser
 router.delete('/:id', auth, checkRole(["admin", "user"]), userController.deleteUser)
 router.post("/otp",userController.otp);
 router.post("/verify", userController.verify)
+router.post("/forget-password", userController.verifyForgetPassword)
 
 module.exports = router
