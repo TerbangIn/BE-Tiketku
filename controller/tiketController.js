85688433cd0fb5213b2d2e521f68c2d0129b4008
zodplugin <zodplugin@gmail.com>
2023-06-22 04:33:30 +0700

    fix tiket

--- controller/tiketController.js
+++ controller/tiketController.js
@@ -99,7 +99,6 @@ const updateTiket = async (req, res) => {
             res.status(200).json({
                 status: 'success',
                 message: `Data dengan index ${id} telah berhasil terupdate`,
-                data
             })
         }
 
