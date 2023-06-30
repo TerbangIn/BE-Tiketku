39e56a9fc12150bf97c7155819f6d6ac0159573c
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-06-30 14:50:54 +0700

    Fix bugs in seat

--- routes/seat.js
+++ routes/seat.js
@@ -5,8 +5,8 @@ const checkRole = require('../middleware/checkRole')
 
 router.get("/", auth, checkRole(["admin", "user"]), seatController.getSeat)
 router.get("/:id", auth, checkRole(["admin", "user"]), seatController.getIdSeat)
-router.post("/", auth, checkRole(["admin", "user"]), seatController.postSeat)
-router.put("/:id", auth, checkRole(["admin"]), seatController.updateSeat)
+router.post("/", auth, checkRole(["admin"]), seatController.postSeat)
+router.put("/:id", auth, checkRole(["admin", "user"]), seatController.updateSeat)
 router.delete("/:id", auth, checkRole(["admin"]), seatController.deleteSeat)
 
 module.exports = router
