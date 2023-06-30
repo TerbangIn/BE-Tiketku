ad0eaab52ee8190fec3325c4fed92ec0159815e0
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-06-30 14:46:58 +0700

    Fix bugs in passenger

--- routes/flight.js
+++ routes/flight.js
@@ -6,7 +6,7 @@ const checkRole = require('../middleware/checkRole')
 router.get("/", flightController.getFlight)
 router.get("/filter/", flightController.filterFlight)
 router.get("/:id", flightController.getIdFlight)
-router.post("/", auth, checkRole(["admin", "user"]), flightController.postFlight)
+router.post("/", auth, checkRole(["admin"]), flightController.postFlight)
 router.put("/:id", auth, checkRole(["admin"]), flightController.updateFlight)
 router.delete("/:id", auth, checkRole(["admin"]), flightController.deleteFlight)
 
--- routes/passenger.js
+++ routes/passenger.js
@@ -5,7 +5,7 @@ const checkRole = require('../middleware/checkRole')
 
 router.get("/", auth, checkRole(["admin", "user"]), passengerController.getPassenger)
 router.get("/:id", auth, checkRole(["admin", "user"]), passengerController.getIdPassenger)
-router.post("/", auth, checkRole(["admin"]), passengerController.postPassenger)
+router.post("/", auth, checkRole(["admin", "user"]), passengerController.postPassenger)
 router.put("/:id", auth, checkRole(["admin"]), passengerController.updatePassenger)
 router.delete("/:id", auth, checkRole(["admin"]), passengerController.deletePassenger)
 
