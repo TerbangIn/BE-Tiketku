fac8a56c68628ddc8cce69a5286ac0bd7c28821a
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-07-03 16:35:26 +0700

    Fix update user

--- seeders/20230609072217-information.js
+++ seeders/20230609072217-information.js
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -20,13 +20,67 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        flight_id: 1,
+        flight_id: 2,
+        name: "Bagasi 2Kg",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 6,
+        name: "Bagasi 2Kg",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 9,
+        name: "Bagasi 2Kg",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 2,
         name: "Free Wifi",
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        flight_id: 1,
+        flight_id: 7,
+        name: "Free Wifi",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 8,
+        name: "Free Wifi",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 9,
+        name: "Free Wifi",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 3,
+        name: "Dessert Box",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 4,
+        name: "Dessert Box",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 6,
+        name: "Dessert Box",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 7,
         name: "Dessert Box",
         createdAt: new Date(),
         updatedAt: new Date()
@@ -38,8 +92,50 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        flight_id: 2,
+        flight_id: 9,
+        name: "Dapet Selimut",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 8,
+        name: "Dapet Selimut",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 4,
+        name: "Dapet Selimut",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 9,
         name: "Free Bar",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 5,
+        name: "Free Bar",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 7,
+        name: "Free Bar",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 3,
+        name: "Free Bar",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 9,
+        name: "Gratis Ciki",
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -48,11 +144,23 @@ module.exports = {
         name: "Gratis Ciki",
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      {
+        flight_id: 8,
+        name: "Gratis Ciki",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        flight_id: 1,
+        name: "Gratis Ciki",
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
